62832f46c76e8a7292f25446a9c4d5c895a8d6cc
Ryan.Y.C.Su <Ryan.Y.C.Su@gmail.com>
2024-05-21 10:10:26 +0800

    Style: change indentation to 4 space

--- src/galois.c
+++ src/galois.c
@@ -28,43 +28,43 @@ static int primitive_poly[] = {
  * @param m    power of the galois field (2^m)
  */
 void galois_init(int m) {
-  if (m < 1 || m > 8) {
-    fprintf(stderr, "galois_init: m should be in [1, 8]\n");
-    exit(1);
-  }
-
-  field_size = 1 << m;
-
-  /* Allocate memory for galois_mul_table and galois_div_table */
-  galois_mul_table = (GF_ELEMENT **)malloc(field_size * sizeof(GF_ELEMENT *));
-  galois_div_table = (GF_ELEMENT **)malloc(field_size * sizeof(GF_ELEMENT *));
-  for (int i = 0; i < field_size; ++i) {
-    galois_mul_table[i] = (GF_ELEMENT *)malloc(field_size * sizeof(GF_ELEMENT));
-    galois_div_table[i] = (GF_ELEMENT *)malloc(field_size * sizeof(GF_ELEMENT));
-  }
-
-  /* Allocate memory for alpha and alpha_inv */
-  alpha = (int *)malloc(field_size * sizeof(int));
-  alpha_inv = (int *)malloc(field_size * sizeof(int));
-
-  /* initialize alpha and alpha_inv */
-  alpha[0] = 1;     // alpha^0 = 1
-  alpha_inv[1] = 0; // 1 = alpha^0
-
-  /* construct galois field */
-  for (int i = 1; i < field_size - 1; ++i) {
-    alpha[i] = alpha[i - 1] << 1; // alpha^i = alpha^(i - 1) * alpha
-
-    /* if i >= m, then alpha[i] >= field_size */
-    if (alpha[i] >= field_size) {
-      alpha[i] ^= primitive_poly[m];
+    if (m < 1 || m > 8) {
+        fprintf(stderr, "galois_init: m should be in [1, 8]\n");
+        exit(1);
     }
-    /* record the mapping from integer to power representation */
-    alpha_inv[alpha[i]] = i;
-  }
 
-  galois_create_mul_table();
-  galois_create_div_table();
+    field_size = 1 << m;
+
+    /* Allocate memory for galois_mul_table and galois_div_table */
+    galois_mul_table = (GF_ELEMENT **)malloc(field_size * sizeof(GF_ELEMENT *));
+    galois_div_table = (GF_ELEMENT **)malloc(field_size * sizeof(GF_ELEMENT *));
+    for (int i = 0; i < field_size; ++i) {
+        galois_mul_table[i] = (GF_ELEMENT *)malloc(field_size * sizeof(GF_ELEMENT));
+        galois_div_table[i] = (GF_ELEMENT *)malloc(field_size * sizeof(GF_ELEMENT));
+    }
+
+    /* Allocate memory for alpha and alpha_inv */
+    alpha = (int *)malloc(field_size * sizeof(int));
+    alpha_inv = (int *)malloc(field_size * sizeof(int));
+
+    /* initialize alpha and alpha_inv */
+    alpha[0] = 1;     // alpha^0 = 1
+    alpha_inv[1] = 0; // 1 = alpha^0
+
+    /* construct galois field */
+    for (int i = 1; i < field_size - 1; ++i) {
+        alpha[i] = alpha[i - 1] << 1; // alpha^i = alpha^(i - 1) * alpha
+
+        /* if i >= m, then alpha[i] >= field_size */
+        if (alpha[i] >= field_size) {
+            alpha[i] ^= primitive_poly[m];
+        }
+        /* record the mapping from integer to power representation */
+        alpha_inv[alpha[i]] = i;
+    }
+
+    galois_create_mul_table();
+    galois_create_div_table();
 }
 
 /*
@@ -73,16 +73,16 @@ void galois_init(int m) {
  * - 1)
  */
 void galois_create_mul_table() {
-  for (int i = 0; i < field_size; ++i) {
-    for (int j = 0; j < field_size; ++j) {
-      if (i == 0 || j == 0) {
-        galois_mul_table[i][j] = 0;
-      } else {
-        int index = (alpha_inv[i] + alpha_inv[j]) % (field_size - 1);
-        galois_mul_table[i][j] = alpha[index];
-      }
+    for (int i = 0; i < field_size; ++i) {
+        for (int j = 0; j < field_size; ++j) {
+            if (i == 0 || j == 0) {
+                galois_mul_table[i][j] = 0;
+            } else {
+                int index = (alpha_inv[i] + alpha_inv[j]) % (field_size - 1);
+                galois_mul_table[i][j] = alpha[index];
+            }
+        }
     }
-  }
 }
 
 /*
@@ -92,22 +92,22 @@ void galois_create_mul_table() {
  * - 1)
  */
 void galois_create_div_table() {
-  for (int i = 0; i < field_size; ++i) {
-    for (int j = 0; j < field_size; ++j) {
-      if (i == 0 || j == 0) {
-        galois_div_table[i][j] = 0;
-      } else {
-        int index = (alpha_inv[i] - alpha_inv[j]) % (field_size - 1);
+    for (int i = 0; i < field_size; ++i) {
+        for (int j = 0; j < field_size; ++j) {
+            if (i == 0 || j == 0) {
+                galois_div_table[i][j] = 0;
+            } else {
+                int index = (alpha_inv[i] - alpha_inv[j]) % (field_size - 1);
 
-        /* if index < 0, then index += field_size - 1 to make it positive */
-        if (index < 0) {
-          index += field_size - 1;
+                /* if index < 0, then index += field_size - 1 to make it positive */
+                if (index < 0) {
+                    index += field_size - 1;
+                }
+
+                galois_div_table[i][j] = alpha[index];
+            }
         }
-
-        galois_div_table[i][j] = alpha[index];
-      }
     }
-  }
 }
 
 /*
@@ -124,26 +124,26 @@ GF_ELEMENT galois_sub(GF_ELEMENT a, GF_ELEMENT b) { return a ^ b; }
  * Galois field multiplication
  */
 GF_ELEMENT galois_mul(GF_ELEMENT a, GF_ELEMENT b) {
-  return galois_mul_table[a][b];
+    return galois_mul_table[a][b];
 }
 
 /*
  * Galois field division
  */
 GF_ELEMENT galois_div(GF_ELEMENT a, GF_ELEMENT b) {
-  return galois_div_table[a][b];
+    return galois_div_table[a][b];
 }
 
 /*
  * Free memory allocated for galois field
  */
 void free_galois() {
-  for (int i = 0; i < field_size; ++i) {
-    free(galois_mul_table[i]);
-    free(galois_div_table[i]);
-  }
-  free(galois_mul_table);
-  free(galois_div_table);
-  free(alpha);
-  free(alpha_inv);
+    for (int i = 0; i < field_size; ++i) {
+        free(galois_mul_table[i]);
+        free(galois_div_table[i]);
+    }
+    free(galois_mul_table);
+    free(galois_div_table);
+    free(alpha);
+    free(alpha_inv);
 }
